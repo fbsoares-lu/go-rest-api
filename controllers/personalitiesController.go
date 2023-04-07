0fe1a23b8088a69444a44acda208397c7ded4879
fbsoares-lu <lsoareshfb@gmail.com>
2023-04-06 21:51:46 -0300

    add PUT method in routes

--- controllers/personalitiesController.go
+++ controllers/personalitiesController.go
@@ -3,23 +3,55 @@ package controllers
 import (
 	"encoding/json"
 	"net/http"
+	"rest-go-api/database"
 	"rest-go-api/models"
-	"strconv"
 
 	"github.com/gorilla/mux"
 )
 
 func FindAll(w http.ResponseWriter, r *http.Request) {
-	json.NewEncoder(w).Encode(models.Personalities)
+	var personalities []models.Personality
+	database.DB.Find(&personalities)
+	json.NewEncoder(w).Encode(personalities)
 }
 
 func FindOne(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	id := vars["id"]
 
-	for _, personality := range models.Personalities {
-		if strconv.Itoa(personality.Id) == id {
-			json.NewEncoder(w).Encode(personality)
-		}
-	}
+	var personality models.Personality
+	database.DB.First(&personality, id)
+
+	json.NewEncoder(w).Encode(personality)
+}
+
+func Create(w http.ResponseWriter, r *http.Request) {
+	var personality models.Personality
+	json.NewDecoder(r.Body).Decode(&personality)
+
+	database.DB.Create(&personality)
+	json.NewEncoder(w).Encode(personality)
+}
+
+func Delete(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id := vars["id"]
+
+	var personality models.Personality
+	database.DB.Delete(&personality, id)
+
+	json.NewEncoder(w).Encode(personality)
+}
+
+func Update(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id := vars["id"]
+
+	var personality models.Personality
+	database.DB.First(&personality, id)
+
+	json.NewDecoder(r.Body).Decode(&personality)
+	database.DB.Save(&personality)
+
+	json.NewEncoder(w).Encode(personality)
 }
--- routes/routes.go
+++ routes/routes.go
@@ -4,14 +4,19 @@ import (
 	"log"
 	"net/http"
 	"rest-go-api/controllers"
+	"rest-go-api/middleware"
 
 	"github.com/gorilla/mux"
 )
 
 func HandleRequest() {
 	r := mux.NewRouter()
+	r.Use(middleware.ContentTypeMiddleware)
 	r.HandleFunc("/", controllers.Home)
 	r.HandleFunc("/api/personalities", controllers.FindAll).Methods("Get")
 	r.HandleFunc("/api/personalities/{id}", controllers.FindOne).Methods("Get")
+	r.HandleFunc("/api/personalities", controllers.Create).Methods("Post")
+	r.HandleFunc("/api/personalities/{id}", controllers.Delete).Methods("Delete")
+	r.HandleFunc("/api/personalities/{id}", controllers.Update).Methods("Put")
 	log.Fatal(http.ListenAndServe(":8000", r))
 }
